837c3219decf54e065f45abd5341f0f6e0db73b4
doomymax <doomymax@gmail.com>
2021-05-13 21:38:48 +0300

    change default value in cli tool

--- src/loader-cli.js
+++ src/loader-cli.js
@@ -11,10 +11,10 @@ export default () => {
     program
       .version('0.0.1', '-v, --version', 'output the current version')
       .arguments('<url>')
-      .option('--output [dirPath]', 'directory for output file')
+      .option('--output [dirPath]', 'directory for output file', process.cwd())
       .action(async (url) => {
         const opts = program.opts();
-        const dirPath = opts.output ?? process.cwd();
+        const dirPath = opts.output;
         await downloadFile(url, dirPath);
       })
       .parse(process.argv)
