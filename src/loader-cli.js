23f90ff9760908e11af53ba949945afc65ed6545
doomymax <doomymax@gmail.com>
2021-05-14 00:25:16 +0300

    add handle error with unknown option

--- src/loader-cli.js
+++ src/loader-cli.js
@@ -7,19 +7,21 @@ export default () => {
 
   config();
 
-  try {
-    program
-      .version('0.0.1', '-v, --version', 'output the current version')
-      .arguments('<url>')
-      .option('--output [dirPath]', 'directory for output file', process.cwd())
-      .action(async (url) => {
-        const opts = program.opts();
-        const dirPath = opts.output;
-        await downloadFile(url, dirPath);
-      })
-      .parse(process.argv)
-      .opts();
-  } catch (e) {
-    console.log('error');
-  }
+  program
+    .version('0.0.1', '-v, --version', 'output the current version')
+    .arguments('<url>')
+    .option('--output [dirPath]', 'directory for output file', process.cwd())
+    .action(async (url) => {
+      const opts = program.opts();
+      const dirPath = opts.output;
+      await downloadFile(url, dirPath);
+    })
+    .configureOutput({
+      writeErr(str) {
+        console.error(str);
+        process.exit(1);
+      },
+    })
+    .parse(process.argv)
+    .opts();
 };
