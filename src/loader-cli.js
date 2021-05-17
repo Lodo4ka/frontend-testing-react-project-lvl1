0444457127768d1f63313ae50540f76a37d58a40
doomymax <doomymax@gmail.com>
2021-05-17 11:06:56 +0300

    handle unknown option

--- src/loader-cli.js
+++ src/loader-cli.js
@@ -10,18 +10,16 @@ export default () => {
   program
     .version('0.0.1', '-v, --version', 'output the current version')
     .arguments('<url>')
+    .exitOverride((err) => {
+      if (err.code) {
+        console.error('something wrong');
+      }
+    })
     .option('--output [dirPath]', 'directory for output file', process.cwd())
     .action(async (url) => {
       const opts = program.opts();
       const dirPath = opts.output;
       await downloadFile(url, dirPath);
     })
-    .configureOutput({
-      writeErr(str) {
-        console.error(str);
-        process.exit(0);
-      },
-    })
-    .parse(process.argv)
-    .opts();
+    .parse(process.argv);
 };
