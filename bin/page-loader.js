9cf5db20a747f5277a879f106389c0f7080812c0
doomymax <doomymax@gmail.com>
2021-05-17 12:40:14 +0300

    refactor call commander in main file

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,27 +1,25 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import program from 'commander';
 import { config } from 'dotenv';
-import downloadFile from '../src/downloadPage';
+import downloadPage from '../src/downloadPage';
 
 config();
 
-try {
-  program
-    .version('0.0.1', '-v, --version', 'output the current version')
-    .arguments('<url>')
-    .option('--output [dirPath]', 'directory for output file', process.cwd())
-    .action(async (url) => {
-      const opts = program.opts();
-      const dirPath = opts.output;
-      try {
-        await downloadFile(url, dirPath);
-      } catch (e) {
-        console.error(e);
-        process.exit(1);
-      }
-    })
-    .parse(process.argv);
-} catch (e) {
-  console.error(e);
-}
+program
+  .description('download all sources from source page')
+  .version('1.0.0', '-v, --version', 'output the current version')
+  .arguments('<url>')
+  .option('-o --output [dirPath]', 'directory for output file', process.cwd())
+  .action(async (url) => {
+    const opts = program.opts();
+    const dirPath = opts.output;
+    try {
+      await downloadPage(url, dirPath);
+      console.log('page has been downloaded');
+    } catch (e) {
+      console.error(e.message);
+      process.exit(1);
+    }
+  })
+  .parse(process.argv);
