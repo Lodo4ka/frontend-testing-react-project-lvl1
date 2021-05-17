f904fc614ba5e4fd34ad29f2a00e50b9a6ebd31d
doomymax <doomymax@gmail.com>
2021-05-17 11:50:47 +0300

    remove module loader-cli

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,5 +1,27 @@
 #!/usr/bin/env node
 
-import loaderCli from '../src/loader-cli.js';
+import { program } from 'commander';
+import { config } from 'dotenv';
+import downloadFile from '../src/downloadPage';
 
-loaderCli();
+config();
+
+try {
+  program
+    .version('0.0.1', '-v, --version', 'output the current version')
+    .arguments('<url>')
+    .option('--output [dirPath]', 'directory for output file', process.cwd())
+    .action(async (url) => {
+      const opts = program.opts();
+      const dirPath = opts.output;
+      try {
+        await downloadFile(url, dirPath);
+      } catch (e) {
+        console.error(e);
+        process.exit(1);
+      }
+    })
+    .parse(process.argv);
+} catch (e) {
+  console.error(e);
+}
