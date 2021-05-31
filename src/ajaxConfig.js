50e9952b97989ffd39c4b683b3c9c60ec797a9fc
doomymax <doomymax@gmail.com>
2021-05-31 20:17:58 +0300

    remove debug from axios

--- src/ajaxConfig.js
+++ src/ajaxConfig.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
-import { addLogger } from 'axios-debug-log';
-import debug from 'debug';
 
 const ajaxConfig = axios.create();
-const ajaxLogger = debug('page-loader');
-addLogger(ajaxConfig, ajaxLogger);
 
 export default ajaxConfig;
