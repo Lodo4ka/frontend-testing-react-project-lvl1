d16361c44ba95f992b60a02fe46aecef54bef0a5
doomymax <doomymax@gmail.com>
2021-06-06 20:14:28 +0300

    refactor test again

--- __tests__/downloadPage.test.js
+++ __tests__/downloadPage.test.js
@@ -5,61 +5,76 @@ import nock from 'nock';
 
 import downloadPage from '../src/index.js';
 
+const readFile = async (pathName) => fs.readFile(pathName, 'utf-8');
+const readFixture = async (pathFixture) => readFile(path.join(__dirname, '..', '__fixtures__', pathFixture));
+
 describe('download page and save in tmp directory', () => {
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  const readFile = async (pathName) => fs.readFile(pathName, 'utf-8');
-  const readFileBinary = async (pathName) => fs.readFile(pathName, 'binary');
-  const readHTML = async (pathName) => {
-    const pathFixture = getFixturePath(pathName);
-    return readFile(pathFixture);
-  };
-  const readAssets = async (pathName) => {
-    const pathFixture = getFixturePath(pathName);
-    return readFileBinary(pathFixture);
-  };
-
-  const url = 'https://ru.hexlet.io/courses';
-  const coursesHtmlName = 'ru-hexlet-io-courses.html';
-  const htmlCoursesname = 'ru-hexlet-io-courses.html';
-  const directoryAssetName = 'ru-hexlet-io-courses_files';
-  const mainDomain = 'https://ru.hexlet.io';
   let tmpDir;
-  let assetDir;
-
   beforeAll(async () => {
     nock.disableNetConnect();
   });
 
   beforeEach(async () => {
     tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-    assetDir = path.join(tmpDir, directoryAssetName);
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
   });
 
   afterAll(() => {
-    nock.cleanAll();
     nock.enableNetConnect();
   });
 
-  test('download html plain, download all imgs from url', async () => {
-    const pngName = '/assets/professions/nodejs.png';
-    const htmlPage = await readHTML(coursesHtmlName);
-    const answerFixtureHTml = await readHTML('ru-hexlet-io-courses-answer.html');
-    const nodePng = await readAssets(pngName);
-    const scopeHtml = nock(mainDomain).get('/courses').reply(200, htmlPage);
-    const scopePng = nock(mainDomain).get(pngName).reply(200, nodePng);
-
-    console.log(downloadPage.toString());
-    await downloadPage(url, tmpDir);
-
-    await expect(fs.access(path.join(tmpDir, htmlCoursesname))).resolves.not.toThrow();
-
-    const directoryPath = await fs.readdir(tmpDir);
-    const assetPath = await fs.readdir(assetDir);
-    const htmlResult = await readFile(path.join(tmpDir, htmlCoursesname));
-
-    expect(directoryPath).toContain(htmlCoursesname);
-    expect(htmlResult).toEqual(answerFixtureHTml);
-    expect(assetPath.length).toEqual(1);
-    expect(assetPath).toContain('ru-hexlet-io-assets-professions-nodejs.png');
+  test('download page from  https://site.com', async () => {
+    const htmlPage = await readFixture('site-com-blog-about.html');
+    const jpg = await readFixture('site-com-photos-me.jpg');
+    const css = await readFixture('site-com-blog-about-assets-styles.css');
+    const js = await readFixture('site-com-assets-scripts.js');
+    const htmlAnswer = await readFixture('/expected/site-com-blog-about.html');
+    nock('https://site.com')
+      .get('/blog/about')
+      .reply(200, htmlPage)
+      .get('/blog/about')
+      .reply(200, htmlPage)
+      .get('/photos/me.jpg')
+      .reply(200, jpg)
+      .get('/assets/scripts.js')
+      .reply(200, js)
+      .get('/blog/about/assets/styles.css')
+      .reply(200, css);
+    await downloadPage('https://site.com/blog/about', tmpDir);
+    const result = await readFile(path.join(tmpDir, 'site-com-blog-about.html'));
+    const directory = await fs.readdir(tmpDir);
+    const directoryAsset = await fs.readdir(path.join(tmpDir, 'site-com-blog-about_files'));
+    expect(result).toEqual(htmlAnswer);
+    expect(directory).toContain('site-com-blog-about.html');
+    expect(directory).toContain('site-com-blog-about_files');
+    expect(directoryAsset).toContain('site-com-photos-me.jpg');
+    expect(directoryAsset).toContain('site-com-blog-about-assets-styles.css');
+    expect(directoryAsset).toContain('site-com-blog-about.html');
+    expect(directoryAsset).toContain('site-com-assets-scripts.js');
   });
+
+  // test('download page from  https://site.com', async () => {
+  //   const htmlPage = await readFixture('site-com-blog-about.html');
+  //   const jpg = await readFixture('site-com-photos-me.jpg');
+  //   const css = await readFixture('site-com-blog-about-assets-styles.css');
+  //   const js = await readFixture('site-com-assets-scripts.js');
+  //   const htmlAnswer = await readFixture('/expected/site-com-blog-about.html');
+  //   nock('https://site.com')
+  //     .get('/blog/about')
+  //     .reply(200, htmlPage)
+  //     .get('/blog/about')
+  //     .reply(200, htmlPage)
+  //     .get('/photos/me.jpg')
+  //     .reply(200, jpg)
+  //     .get('/assets/scripts.js')
+  //     .reply(200, js)
+  //     .get('/blog/about/assets/styles.css')
+  //     .reply(200, css);
+  //   await downloadPage('https://site.com/blog/about', tmpDir);
+  //   const result = await readFile(path.join(tmpDir, 'site-com-blog-about.html'));
+  //   expect(result).toEqual(htmlAnswer);
+  // });
 });
